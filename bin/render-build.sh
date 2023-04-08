05230fed056e95a02c35b1957bae7d3a64c83016
Danaage08 <danaageyt@gmail.com>
2023-04-08 16:37:49 +0300

    dsadsaxsadsadasdsad

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,9 +4,7 @@ set -o errexit
 
 # the current version of Nokogiri ships with incompatible libs
 bundle config --local build.nokogiri --use-system-libraries
-bin/rails credentials:edit (If you have EDITOR set in your $PATH)
-or
-EDITOR="nano" bin/rails credentials:edit
+bin/rails credentials:edit
 
 bundle install
 bundle exec rake assets:precompile
